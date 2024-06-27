d9d082732effd3ed49a43d7c5d8b6642d10f287e
Vedantjn <vedant.jain.indore@gmail.com>
2024-06-27 14:00:40 +0530

    have to update the sidebar further

--- assignment/src/components/HeroSection.jsx
+++ assignment/src/components/HeroSection.jsx
@@ -4,17 +4,22 @@ import About from './About'
 import Border from './Border'
 import ShortInfo from './ShortInfo'
 import CompanyInfo from './CompanyInfo'
+import RightSidebar from './RightSidebar'
+import '../styles/HeroSection.css'
 
 const HeroSection = () => {
   return (
-    <div>
-      <Heading />
-      <Border />
-      <ShortInfo />
-      <Border />
-      <About />
-      <Border />
-      <CompanyInfo />
+    <div className="hero-section-container">
+      <div className="main-content">
+        <Heading />
+        <Border />
+        <ShortInfo />
+        <Border />
+        <About />
+        <Border />
+        <CompanyInfo />
+      </div>
+      <RightSidebar />
     </div>
   )
 }
